5725267b95b953fc0f6b8de5a61c0d8fadea4149
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-02-16 09:33:08 +0700

    Передвижение по карте

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ MAP_FILE = "map.png"
 
 
 class GameView(arcade.Window):
+    def __init__(self, width = 1280, height = 720, title = "Arcade Window"):
+        super().__init__(width, height, title, center_window=True)
+        self.current_position = [37.530887, 55.703118]
+        self.speed = 0.0001  # Скорость передвижения карты
+        
     def setup(self):
         self.get_image()
 
@@ -38,18 +43,19 @@ class GameView(arcade.Window):
         
     def move_map(self, direction: str) -> None:
         if direction == 'left':
-            pass
+            self.current_position[0] -= self.speed
         if direction == 'right':
-            pass
+            self.current_position[0] += self.speed
         if direction == 'up':
-            pass
+            self.current_position[1] += self.speed
         if direction == 'down':
-            pass
+            self.current_position[1] -= self.speed
+        self.get_image()
 
     def get_image(self):
         server_address = 'https://static-maps.yandex.ru/v1?'
         api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-        ll_spn = 'll=37.530887,55.703118&spn=0.002,0.002'
+        ll_spn = f'll={self.current_position[0]},{self.current_position[1]}&spn=0.002,0.002'
         # Готовим запрос.
 
         map_request = f"{server_address}{ll_spn}&apikey={api_key}"
